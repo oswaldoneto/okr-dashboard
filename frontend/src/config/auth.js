4e57caeb9483667a7f5ae88c5df47fcd299beb14
Oswaldo Neto <oswaldo@gmail.com>
2025-04-05 19:01:01 -0300

    Bugfix autenticacao

--- frontend/src/config/auth.js
+++ frontend/src/config/auth.js
@@ -2,15 +2,15 @@ export const msalConfig = {
   auth: {
     clientId: '995158a7-09a1-4d00-99b7-fc1eb2152d96',
     authority: 'https://login.microsoftonline.com/0b96ca9f-6c9e-411d-8d12-fc299ece8307',
-    redirectUri: `${window.location.origin}`,
-    postLogoutRedirectUri: `${window.location.origin}`,
+    redirectUri: window.location.origin,
+    postLogoutRedirectUri: window.location.origin,
     navigateToLoginRequestUrl: true,
     validateAuthority: true
   },
   cache: {
     cacheLocation: 'sessionStorage',
     storeAuthStateInCookie: true,
-    secureCookies: false
+    secureCookies: window.location.protocol === 'https:'
   },
   system: {
     allowNativeBroker: false,
@@ -46,7 +46,7 @@ export const msalConfig = {
 };
 
 export const loginRequest = {
-  scopes: ['User.Read'],
+  scopes: ['User.Read', 'openid', 'profile', 'email'],
   prompt: 'select_account'
 };
 
