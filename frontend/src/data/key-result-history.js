096b8f46e3c6a872d37f5ace512407c8215d7a9c
Oswaldo Neto <oswaldo@gmail.com>
2025-04-10 20:16:35 -0300

    Atualizacao de KRs

--- frontend/src/data/key-result-history.js
+++ frontend/src/data/key-result-history.js
@@ -70,21 +70,21 @@ export const mockKeyResultHistory = {
     { month: "08/2025", value: 0 }
   ],
   "MW-KR-893": [
-    { month: "04/2025", value: 0 },
+    { month: "04/2025", value: 100 },
     { month: "05/2025", value: 0 },
     { month: "06/2025", value: 0 },
     { month: "07/2025", value: 0 },
     { month: "08/2025", value: 0 }
   ],
   "MW-KR-894": [
-    { month: "04/2025", value: 0 },
+    { month: "04/2025", value: 100 },
     { month: "05/2025", value: 0 },
     { month: "06/2025", value: 0 },
     { month: "07/2025", value: 0 },
     { month: "08/2025", value: 0 }
   ],
   "MW-KR-895": [
-    { month: "04/2025", value: 0 },
+    { month: "04/2025", value: 100 },
     { month: "05/2025", value: 0 },
     { month: "06/2025", value: 0 },
     { month: "07/2025", value: 0 },
