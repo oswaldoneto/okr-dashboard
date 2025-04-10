0f86268b1e45c4234e5ea55d98c30be2b02e2e47
Oswaldo Neto <oswaldo@gmail.com>
2025-04-10 11:24:40 -0300

    Atualizacao de KRs

--- frontend/src/data/okrs.js
+++ frontend/src/data/okrs.js
@@ -25,7 +25,7 @@ export const mockOkrs = [
         objectiveId: "MW-O-897",
         title: "Garantir que 95% dos incidentes recebam a primeira resposta dentro do SLA definido (8 horas)",
         dueDate: "30/06/2025",
-        progress: 33,
+        progress: 50,
         disabled: false
       },
       {
@@ -33,7 +33,7 @@ export const mockOkrs = [
         objectiveId: "MW-O-897",
         title: "Garantir que 95% dos incidentes sejam resolvidos dentro do SLA acordado (80 horas)",
         dueDate: "30/06/2025",
-        progress: 60,
+        progress: 100,
         disabled: false
       }
     ]
