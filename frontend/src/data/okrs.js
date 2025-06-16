fa6c0882d6c1c0996ab2908d8ee86a78567f598a
Oswaldo Neto <oswaldo@gmail.com>
2025-06-16 10:23:07 -0300

    Update KRs

--- frontend/src/data/okrs.js
+++ frontend/src/data/okrs.js
@@ -57,7 +57,7 @@ export const mockOkrs = [
         objectiveId: "MW-O-884",
         title: "Manter os custos de infraestrutura para operação DIO abaixo de R$ 10.000 sem comprometer performance",
         dueDate: "30/06/2025",
-        progress: 70,
+        progress: 69,
         disabled: false,
         currentValue: 14220.00
       },
